96366fab0214f3eec142d183dff1f82e517dde64
lmeyer <lmeyer@student.42.fr>
2019-02-21 12:37:27 +0100

    speed independant of timeframe

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 let t;
+let timer;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	frameRate(60);
 	t = new Transform();
+	timer = new Timer();
 }
 
 function draw() {
-	push();
-	fill(30);
-	rect(0, 0, width, height);
-	pop();
+	timer.update();
 
-	const step = 10;
+	const stepPerSec = 500;
+	let step = stepPerSec * timer.delta;
 	if (keyIsDown(DOWN_ARROW)) {
 		let translated = new Transform(t.x, t.y + step);
 		if (translated.y < height)
@@ -35,6 +35,11 @@ function draw() {
 	}
 
 	push();
+	fill(30);
+	rect(0, 0, width, height);
+	pop();
+
+	push();
 	fill(127);
 	translate(t.x, t.y);
 	triangle(-5, -10, 0, 10, 5, -10);
@@ -52,5 +57,18 @@ class Transform {
 	constructor(x = 0, y = 0) {
 		this.x = x;
 		this.y = y;
+	}
+}
+
+class Timer {
+	constructor() {
+		this.last = millis();
+		this.delta = 0;
+	}
+
+	update () {
+		let now = millis();
+		this.delta = (now - this.last) / 1000;
+		this.last = now;
 	}
 }
