f75b68f8e488dd3647e379f907c8880fcb362843
lmeyer <lmeyer@student.42.fr>
2019-02-21 13:50:10 +0100

    refactor with World and Player

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let t;
+let world;
 let timer;
 
 function setup() {
@@ -6,47 +6,21 @@ function setup() {
 	frameRate(60);
 	angleMode(DEGREES);
 
-	t = new Transform();
+	world = new World();	
 	timer = new Timer();
 }
 
 function draw() {
 	timer.update();
+	world.update();
 
-	const translationStepPerSec = 100;
-	let translationStep = translationStepPerSec * timer.delta;
-	const rotationStepPerSec = 180;
-	let rotationStep = rotationStepPerSec * timer.delta;
-	if (keyIsDown(RIGHT_ARROW)) {
-		t.rotate(-rotationStep);
-	}
-	if (keyIsDown(LEFT_ARROW)) {
-		t.rotate(rotationStep);
-	}
-	if (keyIsDown(DOWN_ARROW)) {
-		let xTranslation = translationStep * cos(t.rotation);
-		let yTranslation = -translationStep * sin(t.rotation);
-		let translated = new Transform(t.x - xTranslation, t.y - yTranslation, t.rotation);
-		t = translated;
-	}
-	if (keyIsDown(UP_ARROW)) {
-		let xTranslation = translationStep * cos(t.rotation);
-		let yTranslation = -translationStep * sin(t.rotation);
-		let translated = new Transform(t.x + xTranslation, t.y + yTranslation, t.rotation);
-		t = translated;
-	}
 
 	push();
 	fill(30);
 	rect(0, 0, width, height);
 	pop();
 
-	push();
-	fill(127);
-	translate(t.x, t.y);
-    rotate(-90 - t.rotation);
-	triangle(-5, -10, 0, 10, 5, -10);
-	pop();
+	world.draw();
 
 	push();
 	var fps = frameRate();
@@ -86,5 +60,64 @@ class Timer {
 		let now = millis();
 		this.delta = (now - this.last) / 1000;
 		this.last = now;
+	}
+}
+
+class World {
+    constructor() {
+        this.player = new Player ();
+    }
+
+    update() {
+        this.player.update();
+    }
+
+    draw() {
+        this.player.draw();
+	}
+}
+
+class Player {
+	constructor(transform = new Transform()) {
+		this.transform = transform;
+		this.translationStepPerSec = 100;
+		this.rotationStepPerSec = 180;
+		this.color = color(255, 204, 0);
+	}
+
+	update() {
+		let translationStep = this.translationStepPerSec * timer.delta;
+		let rotationStep = this.rotationStepPerSec * timer.delta;
+		if (keyIsDown(RIGHT_ARROW)) {
+			this.transform.rotate(-rotationStep);
+		}
+		if (keyIsDown(LEFT_ARROW)) {
+			this.transform.rotate(rotationStep);
+		}
+		if (keyIsDown(DOWN_ARROW)) {
+			let xTranslation = translationStep * cos(this.transform.rotation);
+			let yTranslation = -translationStep * sin(this.transform.rotation);
+			let translated = new Transform(this.transform.x - xTranslation,
+				this.transform.y - yTranslation,
+				this.transform.rotation);
+			this.transform = translated;
+		}
+		if (keyIsDown(UP_ARROW)) {
+			let xTranslation = translationStep * cos(this.transform.rotation);
+			let yTranslation = -translationStep * sin(this.transform.rotation);
+			let translated = new Transform(this.transform.x + xTranslation,
+				this.transform.y + yTranslation,
+				this.transform.rotation);
+			this.transform = translated;
+		}
+	}
+
+	draw() {
+		push();
+		fill(this.color);
+		translate(this.transform.x, this.transform.y);
+		rotate(-90 - this.transform.rotation);
+		triangle(-5, -10, 0, 10, 5, -10);
+		pop();
 	}
 }
